54484415a6a3b91daef41ae2ad101c088a0657e0
leap <lipubest@gmail.com>
2013-03-08 17:16:38 +0800

    合并最新版本代码

---
+++ tests/util.js
@@ -0,0 +1,35 @@
+/**
+ * Requires
+ */
+
+var util = require('util')
+  , Stream = require('stream');
+
+/**
+ * Export TestStream
+ */
+
+var TestStream = module.exports.TestStream = 
+function TestStream() {
+  if (!this instanceof TestStream) return new TestStream();
+
+  this.encoding = null;
+
+  Stream.apply(this);
+};
+util.inherits(TestStream, Stream);
+
+TestStream.prototype.write = function(buffer) {
+  if (this.encoding) {
+    buffer = buffer.toString(this.encoding);
+  }
+  process.nextTick(this.emit.bind(this, 'data', buffer));
+};
+
+TestStream.prototype.setEncoding = function(encoding) {
+  this.encoding = encoding;
+};
+
+TestStream.prototype.end = function() {
+  this.emit('close');
+};
